55433384720da8ee01de0d1a97cee8838c0bf7b7
Unknown <mirirubi6@gmail.com>
2018-04-12 01:50:33 +0200

    Auto stash before merge of "master" and "origin/master"

--- public/cliente.js
+++ public/cliente.js
@@ -1,16 +1,4 @@
-var socket = io.connect("http://localhost:8080");
-var COLORES = [
-	'#7D3C98', '#2471A3', '#17A589', '#A93226',
-	'#229954', '#F1C40F', '#BA4A00', '#DF01A5',
-	'#B9770E', '#1C2833'
-];
-socket.on('getUsernameColor', function (nombre) {
-	//function getUsernameColor(username) {
-	var index = client.color;
-	console.log(index);
-	console.log(COLORS[index]);
-	return COLORS[0];
-});
+var socket = io.connect();
 
 
 socket.on('connect', function (datos) {
@@ -40,68 +28,67 @@ socket.on('usuario no valido', function (datos) {
 });
 
 socket.on('borrar', function (nombre) {
-	$('#chat').append(nombre + ' se ha desconectado\n');
-	//$('#chat').append($('<div>').css('font-style', 'oblique').text(nombre + ' se ha desconectado\n')).append($('\n')); // se supone que esto sirve para añadir estilos,
-	//pero lo que hace es crearme:
-	//<div style = 'font-style: 'oblique';'>lodedentrodeltext()</div>'
-	//$('#chat').append($('<div>').css('font-style', 'italic').text(nombre + ' se ha desconectado\n'));
+	$('#chat').append('<li><p>' + nombre + ' se ha desconectado </p></li>');
 });
 
 socket.on('unir', function (nombre) {
-	$('#chat').append(nombre + ' se ha unido\n');
+	$('#chat').append('<li><p>' + nombre + ' se ha unido </p></li>');
 });
 
 /* socket.on('mensajes',function(datos){
-	$('#chat').append(datos.info+'\n');
+  $('#chat').append(datos.info+'\n');
 }); */
 socket.on('output', function (data) {
 	console.log(data);
 	if (data.length) {
 		for (var x = 0; x < data.length; x++) {
-			//$('#chat').append($('<div>').text(data[x].nickname)).append($(':')).append(data[x].mensaje).append($('\n'));
-			$('#chat').append(data[x].nickname + ':' + data[x].mensaje + '\n');
+			$('#chat').append('<li><p>' + data[x].nickname + ': ' + data[x].mensaje + '</p></li>');
 		}
+		$('#chat')[0].scrollTop = $('#chat')[0].scrollHeight;
 	}
 });
 
-socket.on('refrescar usuarios', function (usuarios) {
-	$('.list-unstyled').html(usuarios);
-});
+
 
 $(document).ready(function () {
-
+	$('.users').scrollTop($('.users').height());
+	$('#chat').scrollTop($('#chat').height());
 
 	$('.enviar').on('click', function (e) {
 		if ($('.mensaje').val() != "") {
 			socket.emit('mensajes', { nickname: socket.nickname, mensaje: $('.mensaje').val() });
-			$('#chat').append(socket.nickname + ':' + $('.mensaje').val() + '\n');
+			$('#chat').append('<li><p>Yo: ' + $('.mensaje').val() + '</p></li>');
+			$('#chat')[0].scrollTop = $('#chat')[0].scrollHeight;
 			$('.mensaje').val('');
-		}
-		/*if ($('.mensaje').val() != "") {
-			socket.emit('mensajes', { nickname: socket.nickname, mensaje: $('.mensaje').val() });
-			$('#chat').append(socket.nickname.css('color', getUsernameColor(socket.nickname)) + ': ' + $('.mensaje').val() + '\n');//.css('color', '#7D3C98')
-			$('.mensaje').val('');
-		} */else {
+		} else {
 			alertify.error("Introduce un mesaje");
 		}
 
 	});
 	$('.mensaje').keypress(function (e) {
-		if ((e.which == 13) && ($('.mensaje').val() != "")) {
+		if ((e.which == 13) && ($('#mensaje1').val() != "")) {
 			socket.emit('mensajes', { nickname: socket.nickname, mensaje: $('.mensaje').val() });
-			$('#chat').append(socket.nickname + ':' + $('.mensaje').val() + '\n');
-			$('#chat').append(socket.nickname + ':' + $('.mensaje').val() + '\n');
+			$('#chat').append('<li><p>Yo: ' + $('.mensaje').val() + '</p></li>');
+			$('#chat')[0].scrollTop = $('#chat')[0].scrollHeight;
+			$('.mensaje').val('');
 
-			$('#chat').append($('<div>').css('color', '#7d3c98').text(socket.nickname)).append($('\n')); // se supone que esto sirve para añadir estilos,
-			//pero lo que hace es crearme:
-			//<div style = 'color: rgb(125, 60, 152);'>lodedentrodeltext()</div>'
-			//$('.mensaje').val('');
-			/*$('#chat')
-				.append($('<span>').css('color', '#7D3C98').text(socket.nickname))
-				.append($('<br>'));*/
-			//$('.mensaje').val('');
 
 		} else {
 		}
+	});
+
+
+	$('.mensaje').keypress(function (e) {
+		console.log('dasd');
+		socket.emit('escribiendo', { nickname: socket.nickname, escribiendo: true });
+	});
+
+
+	setInterval(function () {
+		socket.emit('escribiendo', { nickname: socket.nickname, escribiendo: false });
+	}, 2000);
+
+	$('#emoji').on('click', function () {
+		$('#emoji').val($('.mensaje').val());
 	});
 });
